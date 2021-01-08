5cb84a008f97d8a6bdf979c761cf122d941ad4cf
luiz.higuti <luiz_higuti@hotmail.com>
2021-01-08 16:53:34 -0300

    add login validator methods

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import React, { CSSProperties } from 'react';
+import React, { useState, CSSProperties } from 'react';
 import './App.css';
-import { EmailInput, PasswordInput, SubmitButton } from './components/login';
+import { EmailInput, PasswordInput, SubmitButton } from './components/login'
+import { Validate } from './components/login-validator';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleErrors () {
+    const errors = Validate(email, password)
+    if (errors.length > 0) {
+      alert(errors);
+    }
+  }
+
   return (
     <div className='App'>
       <h1>Bem-vindo(a) à Taqtile!</h1>
       <form style= {formStyles}>
-        <EmailInput />
-        <PasswordInput />
-        <SubmitButton />
+        <EmailInput text={email} onTextChange={ (input) => {setEmail(input)} } />
+        <PasswordInput text={password} onTextChange= { (input) => {setPassword(input)} } />
+        <SubmitButton validate={ handleErrors } />
       </form>
+      
     </div>
   );
 }
--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,39 +1,72 @@
 import React, { CSSProperties } from 'react';
 
-export class EmailInput extends React.Component {
+interface LoginProps {
+  text: string;
+  onTextChange: (e: string) => void;
+}
+
+interface ButtonProps {
+  validate: () => void;
+}
+
+export class EmailInput extends React.Component<LoginProps> {
+  constructor(props: LoginProps) {
+    super(props);
+  }
+
   render() {
     return (
       <>
         <label htmlFor="email">Email</label>
-        <input type='text' name='email' id="email" style={inputStyles}/>
+        <input
+          type='text'
+          name='email'
+          id='email'
+          style={inputStyles}
+          onChange={e => {this.props.onTextChange(e.target.value)}}
+          value={this.props.text}
+        />
       </>
-
     );
   }
 }
 
-export class PasswordInput extends React.Component {
+
+export class PasswordInput extends React.Component<LoginProps> {
+  constructor(props: LoginProps) {
+    super(props);
+  }
+
   render() {
     return (
       <>
-        <label htmlFor="password">Password</label>
-        <input type='text' name='email' id="password" style={inputStyles}/>
+        <label htmlFor="password">Senha</label>
+        <input
+          type='text'
+          name='password'
+          id='password'
+          style={inputStyles}
+          onChange={e => {this.props.onTextChange(e.target.value)}}
+          value={this.props.text}
+        />
       </>
     );
   }
 }
 
-export class SubmitButton extends React.Component {
+export class SubmitButton extends React.Component<ButtonProps> {
   render() {
-    return <button style={buttonStyles}>Entrar</button>;
+    return (
+      <button style={buttonStyles} onClick={this.props.validate}>Entrar</button>
+    );
   }
+}
+
+const inputStyles: CSSProperties = {
+  marginBottom: '15px',
 }
 
 const buttonStyles: CSSProperties = {
   backgroundColor: 'blue',
   color: 'white',
 };
-
-const inputStyles: CSSProperties = {
-  marginBottom: '15px',
-}
