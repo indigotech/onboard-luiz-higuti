dc66ac1cd2fc844b37d7ac24c2d583463e7b068c
luiz.higuti <luiz_higuti@hotmail.com>
2021-01-12 16:24:47 -0300

    add users list page

--- src/users-list.tsx
+++ src/users-list.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 import { getUsers, UsersList } from './components/users';
 import { buttonStyles } from './components/login';
@@ -41,3 +42,58 @@ export const UsersListPage: React.FC = () => {
 };
 
 
+=======
+import React from 'react';
+
+interface UserProps {
+	name: string
+	email: string,
+}
+
+interface UsersListProps {
+	list: Array<UserProps>
+}
+
+const User:React.FC<UserProps> = (props) => {
+	return (
+		<li>
+			<p>
+				{props.name}
+			</p>
+			<p>
+				{props.email}
+			</p>
+
+		</li>
+	);
+};
+
+const UsersList:React.FC<UsersListProps> = (props) => {
+  return(
+		<ul>
+			{ props.list.map(p => <User email={p.email} name={p.name} key={p.name}/>)
+			}
+		</ul>	
+	);
+};
+
+export const UsersListPage: React.FC = () => {
+	const users = [
+		{
+			name: 'user1',
+			email: 'email1'
+		},
+		{
+			name: 'user2',
+			email: 'email2'
+		}
+	]
+
+	return (
+		<div>
+			<h1>Users List</h1>
+			<UsersList list={users} />
+		</div>
+	)
+}
+>>>>>>> 91d5e16... add users list page
