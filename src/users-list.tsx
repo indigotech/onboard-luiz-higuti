ef1221e9ab0639dd9c56dffff42cb65d71667f91
luiz.higuti <luiz_higuti@hotmail.com>
2021-01-12 16:40:44 -0300

    format users-list.tsx

--- src/users-list.tsx
+++ src/users-list.tsx
@@ -1,53 +1,49 @@
 import React from 'react';
 
 interface UserProps {
-	name: string
-	email: string,
+  name: string;
+  email: string;
 }
 
 interface UsersListProps {
-	list: Array<UserProps>
+  list: Array<UserProps>;
 }
 
-const User:React.FC<UserProps> = (props) => {
-	return (
-		<li>
-			<p>
-				{props.name}
-			</p>
-			<p>
-				{props.email}
-			</p>
-
-		</li>
-	);
+const User: React.FC<UserProps> = (props) => {
+  return (
+    <li>
+      <p>{props.name}</p>
+      <p>{props.email}</p>
+    </li>
+  );
 };
 
-const UsersList:React.FC<UsersListProps> = (props) => {
-  return(
-		<ul>
-			{ props.list.map(p => <User email={p.email} name={p.name} key={p.name}/>)
-			}
-		</ul>	
-	);
+const UsersList: React.FC<UsersListProps> = (props) => {
+  return (
+    <ul>
+      {props.list.map((p) => (
+        <User email={p.email} name={p.name} key={p.name} />
+      ))}
+    </ul>
+  );
 };
 
 export const UsersListPage: React.FC = () => {
-	const users = [
-		{
-			name: 'user1',
-			email: 'email1'
-		},
-		{
-			name: 'user2',
-			email: 'email2'
-		}
-	]
+  const users = [
+    {
+      name: 'user1',
+      email: 'email1',
+    },
+    {
+      name: 'user2',
+      email: 'email2',
+    },
+  ];
 
-	return (
-		<div>
-			<h1>Users List</h1>
-			<UsersList list={users} />
-		</div>
-	)
-}
+  return (
+    <div>
+      <h1>Users List</h1>
+      <UsersList list={users} />
+    </div>
+  );
+};
